783a961a71bc8b2ff9f9bedea97c3c71fc044299
mst101097 <singh.1097.mohit@gmail.com>
2021-07-28 21:21:08 +0530

    adding String Problem

---
+++ String_Problems/findingBracketBalancedString.py
@@ -0,0 +1,50 @@
+'''
+Check for Balanced Brackets in an expression (well-formedness) using Stack
+Given an expression string exp, write a program to examine
+whether the pairs and the orders of “{“, “}”, “(“, “)”, “[“, “]” are correct in exp.
+
+Input: exp = “[()]{}{[()()]()}”
+Output: Balanced
+
+Input: exp = “[(])”
+Output: Not Balanced
+
+'''
+def findBracketBalance(str1):
+
+	stack = []
+
+	for char in str1:
+
+		if char in ['[','{','(']:
+			stack.append(char)
+
+		else:
+			if not stack:
+				return False
+
+			currentChar = stack.pop()
+
+			if currentChar =='[':
+				if char != ']':
+					return False
+
+			if currentChar =='{':
+				if char != '}':
+					return False
+
+			if currentChar =='(':
+				if char != ')':
+					return False
+	if stack:
+		return False
+	return True
+
+inputStr = "{()[]"
+
+if findBracketBalance(inputStr):
+	print('Balanced String ')
+else:
+	print('Not Balanced')
+
+
