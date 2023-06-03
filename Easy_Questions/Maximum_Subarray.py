9162a7e972876e35bd66a6f21dc8901514a7eff2
mst101097 <singh.1097.mohit@gmail.com>
2023-06-03 14:08:55 +0530

    Add Maximum_Subarray Problem

--- Easy_Questions/Maximum_Subarray.py
+++ Easy_Questions/Maximum_Subarray.py
@@ -1,16 +1,17 @@
 class Soluation:
-    def maxSubArray(self, nums : List[int]) -> int:
-        masSub = nums[0]
+    def maxSubArray(self, nums : list[int]) -> int:
+        maxSub = nums[0]
         currSum = 0
 
         for n in nums:
             if currSum < 0:
                 currSum = 0
             currSum += n 
-        maxSub = max(maxSub, currSum)
+            maxSub = max(maxSub, currSum)
 
-        return masSub
+        return maxSub
  
 list1 = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
 obj = Soluation()
-obj.maxSubArray(list1)
+returnSubArray = obj.maxSubArray(list1)
+print(returnSubArray)
